afb2352ee7806565a3a7c781f9ec524e413c9dc6
Bernardo Esbérard <bernardbr@gmail.com>
2022-05-05 23:19:42 -0300

    :memo: Adicionado TODO

--- src/controleFrota/Impl/Autenticador.java
+++ src/controleFrota/Impl/Autenticador.java
@@ -2,6 +2,8 @@ package controleFrota.Impl;
 
 import controleFrota.Autenticavel;
 
+// TODO: Criar lista de autenticados e implementar o método autenticar.
+
 public class Autenticador {
     public boolean autenticar(Autenticavel autenticavel) {
         String credenciais = autenticavel.obterCredenciais();
